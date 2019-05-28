a0db08e07f9932e630ae51e52436096ad65506cf
Python-Development <legromant@gmail.com>
2019-05-28 16:08:01 +0300

    test

--- lesson_1.py
+++ lesson_1.py
@@ -11,8 +11,3 @@ print(weekdays.index('Friday'))
 
 print('Friday' in weekdays)
 
-another_empty_list = list('dog')
-print(another_empty_list)
-
-a_tuple = ('ready', 'fire', 'aim')
-print(list(a_tuple))
