c553a5459445233ecb748360a1f47dce5377fd3d
Python-Development <legromant@gmail.com>
2019-05-28 15:24:49 +0300

    Initial commit

---
+++ lesson_1.py
@@ -0,0 +1,18 @@
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+print(weekdays)
+
+weekdays.append('Wood')
+print(weekdays)
+
+print(weekdays.pop())
+print(weekdays)
+
+print(weekdays.index('Friday'))
+
+print('Friday' in weekdays)
+
+another_empty_list = list('dog')
+print(another_empty_list)
+
+a_tuple = ('ready', 'fire', 'aim')
+print(list(a_tuple))
