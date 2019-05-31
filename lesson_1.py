e0688be2a1698840d4b2494f872eb05669325bfa
Python-Development <legromant@gmail.com>
2019-05-31 20:12:54 +0300

    "Hello, World!"

--- lesson_1.py
+++ lesson_1.py
@@ -1,13 +1,1 @@
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-print(weekdays)
-
-weekdays.append('Wood')
-print(weekdays)
-
-print(weekdays.pop())
-print(weekdays)
-
-print(weekdays.index('Friday'))
-
-print('Friday' in weekdays)
-
+print('"Hello, World!"')
