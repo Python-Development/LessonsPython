cfc23f3038def196c6d4ba2de0bdefadfe7bbc62
Python-Development <legromant@gmail.com>
2019-06-04 21:15:04 +0300

    "Hello, World!"

--- lesson_1.py
+++ lesson_1.py
@@ -1,12 +1,1 @@
 print('"Hello, World!"')
-
-step = '#'
-height = 0
-
-print('height: 8')
-while height != 8:
-    for i in range(8):
-        step += '#'
-        print(step.rjust(9) + step.ljust(9))
-        height += 1
-
